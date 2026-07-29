cb8a98fe64e4ca83745460144c56e76e6ac47e3b
AccelAnalysis <jholman@accelanalysis.com>
2026-07-29 16:15:28 -0400

    AUTH-002: export server resolver composition

--- src/infrastructure/auth/index.ts
+++ src/infrastructure/auth/index.ts
@@ -2,4 +2,5 @@ export * from "./firebase-browser";
 export * from "./firebase-client";
 export * from "./firebase-server";
 export * from "./firebase-user-resolution";
+export * from "./firebase-user-resolution-server";
 export * from "./provider";
