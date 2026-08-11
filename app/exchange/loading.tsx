9ea7e199d69a9e14dd1a4aa8b6e7a361969cfa60
AccelAnalysis <jholman@accelanalysis.com>
2026-08-10 21:38:07 -0400

    feat: scope Exchange entry loading below the shell

---
+++ app/exchange/loading.tsx
@@ -0,0 +1,5 @@
+import { ParticipantContentLoading } from "@/src/components/participant/ParticipantContentLoading";
+
+export default function ExchangeEntryLoading() {
+  return <ParticipantContentLoading target="exchange-entry" />;
+}
