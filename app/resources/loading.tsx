a66144a5d015714df76f528373da629ca5351217
AccelAnalysis <jholman@accelanalysis.com>
2026-08-10 21:37:38 -0400

    feat: scope Resources loading below the shell

---
+++ app/resources/loading.tsx
@@ -0,0 +1,5 @@
+import { ParticipantContentLoading } from "@/src/components/participant/ParticipantContentLoading";
+
+export default function ResourcesLoading() {
+  return <ParticipantContentLoading target="resources" />;
+}
