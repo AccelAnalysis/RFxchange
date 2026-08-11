4ea2ec890fe1a5aac5eca321052acf24571537c5
AccelAnalysis <jholman@accelanalysis.com>
2026-08-10 21:38:01 -0400

    feat: scope provider application loading below the shell

---
+++ app/provider-application/loading.tsx
@@ -0,0 +1,5 @@
+import { ParticipantContentLoading } from "@/src/components/participant/ParticipantContentLoading";
+
+export default function ProviderApplicationLoading() {
+  return <ParticipantContentLoading target="provider-application" />;
+}
